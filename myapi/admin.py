bef20cb862731608c82dab825c38a7a66d9fd97c
Emmanuel Praise <emmapraise@gmail.com>
2021-07-13 15:51:18 +0100

    Update admin.py

--- myapi/admin.py
+++ myapi/admin.py
@@ -4,4 +4,4 @@ from django.contrib.auth.admin import UserAdmin
 from myapi.models import *
 # Register your models here.
 admin.site.register(User, UserAdmin,)
-admin.site.register([ Category, Product])
+admin.site.register([ Category, Product, Cart, Shipping, Saved, Payment, Sales])
