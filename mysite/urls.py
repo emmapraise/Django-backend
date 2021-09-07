433b855b1f12818a64d17a2f02f95c26c8f11141
Emmanuel Praise <emmapraise@gmail.com>
2021-09-07 20:59:07 +0100

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Etsea API",
+      title="Etsea APIs",
       default_version='v1',
       description="Etsea API documentation",
       terms_of_service="https://www.google.com/policies/terms/",
