20347d64ea2c95ef25f79027bccd7760a46044a7
Emmanuel Praise <emmapraise@gmail.com>
2021-09-26 11:49:22 +0100

    FIX: Test

--- myapi/test/test_views.py
+++
@@ -1,7 +0,0 @@
-from .test_setup import TestSetUp
-
-class TestViews(TestSetUp):
-
-    def test_user_can_register(self):
-        res = self.client.post(self.register_url)
-        res.status_code == 400
